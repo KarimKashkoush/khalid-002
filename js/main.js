34b7d4cef083b390404926d31ec7ff50090df519
karimKashkoush <karimkashkoush5@gmail.com>
2024-10-09 08:21:35 +0300

    add

--- js/main.js
+++ js/main.js
@@ -51,8 +51,7 @@ var swiper = new Swiper(".mySwiper", {
 var swiper2 = new Swiper(".mySwiper2", {
     slidesPerView: 5,
     spaceBetween: 30,
-    loop: true,
-    centeredSlides: true,
+    freeMode: true,
     
     // Adding smoothness
     speed: 800, // Adjust the speed (in milliseconds)
